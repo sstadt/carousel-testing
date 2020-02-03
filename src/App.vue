6b7d2c9472309aaa7090b5b427199ef5b4e03c47
Scott Stadt <scott.stadt@gmail.com>
2020-02-03 10:57:39 -0500

    tinkering with vue-awesome-swiper

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,70 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <swiper :options="swiperOptions" ref="mySwiper" @someSwiperEvent="callback">
+      <!-- slides -->
+      <swiper-slide>I'm Slide 1</swiper-slide>
+      <swiper-slide>I'm Slide 2</swiper-slide>
+      <swiper-slide>I'm Slide 3</swiper-slide>
+      <swiper-slide>I'm Slide 4</swiper-slide>
+      <swiper-slide>I'm Slide 5</swiper-slide>
+      <swiper-slide>I'm Slide 6</swiper-slide>
+      <swiper-slide>I'm Slide 7</swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+      <div class="swiper-button-prev" slot="button-prev"></div>
+      <div class="swiper-button-next" slot="button-next"></div>
+      <div class="swiper-scrollbar"   slot="scrollbar"></div>
+    </swiper>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: 'carrousel',
+    // data() {
+    //   return {
+    //     swiperOption: {}
+    //   }
+    // },
+    computed: {
+      swiper() {
+        return this.$refs.mySwiper.swiper
+      },
+      swiperOptions() {
+        return {
+          slidesPerView: 3,
+          loop: true,
+          navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+          breakpoints: {
+            768: {
+              slidesPerView: 1
+            }
+          }
+        }
+      }
+    },
+    mounted() {
+      console.log('this is current swiper instance object', this.swiper)
+      // this.swiper.slideTo(3, 1000, false)
+    },
+    methods: {
+      callback() {
+        console.log('foo')
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    max-width: 800px;
+    margin: 0 auto;
+  }
+
+  .swiper-slide {
+    min-height: 300px;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import VueMq from 'vue-mq'
 
 Vue.config.productionTip = false
 
+Vue.use(VueAwesomeSwiper)
+
+Vue.use(VueMq, {
+  breakpoints: {
+    mobile: 768,
+    tablet: 1024,
+    desktop: 1280,
+    hd: Infinity,
+  },
+})
+
+require('swiper/dist/css/swiper.min.css')
+
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
